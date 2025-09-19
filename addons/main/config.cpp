c960d71bf3720684ff8cc3743a3f058af2db5903
OverlordZorn <dennisAzorn@gmail.com>
2025-09-20 01:52:02 +0200

    Update config.cpp

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -17,7 +17,7 @@ class CfgPatches {
 
         // Required addons, used for setting load order.
         // When any of the addons is missing, pop-up warning will appear when launching the game.
-        requiredAddons[] = {"cba_main"};
+        requiredAddons[] = {};
 
         // List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content (units and groups)
         units[] = {};
