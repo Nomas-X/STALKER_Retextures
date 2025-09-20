29216dd77fabd5e2d31beb9d505ab6072d39523c
OverlordZorn <dennisAzorn@gmail.com>
2025-09-20 10:18:04 +0200

    init military addon

---
+++ addons/military/script_component.hpp
@@ -0,0 +1,8 @@
+#define COMPONENT military
+
+// This is being used for the Addon's Name and can be "My Addon Template Framework"
+#define COMPONENT_BEAUTIFIED Military Faction
+
+
+#include "\z\stalker_retextures\addons\main\script_mod.hpp"
+#include "\z\stalker_retextures\addons\main\script_macros.hpp"
