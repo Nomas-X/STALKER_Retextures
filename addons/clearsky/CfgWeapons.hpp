77c715c43efe48dc12956ddc2d955b799e91b5ea
Nomas <nomasx2000@gmail.com>
2025-09-25 11:27:05 +0300

    Added all altyn variants to Clear Sky

--- addons/clearsky/CfgWeapons.hpp
+++ addons/clearsky/CfgWeapons.hpp
@@ -27,4 +27,53 @@ class cfgWeapons
 
 		hiddenSelectionsTextures[] = { QPATHTOF(data\rhs_altyn_black_co.paa) };
 	};
+
+	class rhs_altyn_novisor;
+	class GVAR(H_altyn_novisor_black): rhs_altyn_novisor {
+		author = "RHS, Rad";
+		displayName = "Altyn [Clearsky]";
+
+		hiddenSelectionsTextures[] = { QPATHTOF(data\rhs_altyn_black_co.paa) };
+	};
+
+	class rhs_altyn_novisor_bala;
+	class GVAR(H_altyn_novisor_bala_black): rhs_altyn_novisor_bala {
+		author = "RHS, Rad";
+		displayName = "Altyn (Balaclava) [Clearsky]";
+
+		hiddenSelectionsTextures[] = { QPATHTOF(data\rhs_altyn_black_co.paa) };
+	};
+
+	class rhs_altyn_novisor_ess_bala;
+	class GVAR(H_altyn_novisor_ess_bala_black): rhs_altyn_novisor_ess_bala {
+		author = "RHS, Rad";
+		displayName = "Altyn (ESS/Balaclava) [Clearsky]";
+
+		hiddenSelectionsTextures[] = { QPATHTOF(data\rhs_altyn_black_co.paa) };
+	};
+
+	class rhs_altyn_visordown;
+	class GVAR(H_altyn_visordown_black): rhs_altyn_visordown {
+		author = "RHS, Rad";
+		displayName = "Altyn (Visor Down) [Clearsky]";
+
+		hiddenSelectionsTextures[] = { QPATHTOF(data\rhs_altyn_black_co.paa) };
+	};
+
+	class rhs_altyn;
+	class GVAR(H_altyn_black): rhs_altyn {
+		author = "RHS, Rad";
+		displayName = "Altyn (Visor Up) [Clearsky]";
+
+		hiddenSelectionsTextures[] = { QPATHTOF(data\rhs_altyn_black_co.paa) };
+	};
+
+	class rhs_altyn_bala;
+	class GVAR(H_altyn_bala_black): rhs_altyn_bala {
+		author = "RHS, Rad";
+		displayName = "Altyn (Visor Up/Balaclava) [Clearsky]";
+
+		hiddenSelectionsTextures[] = { QPATHTOF(data\rhs_altyn_black_co.paa) };
+	};
+
 };
