b6e0ad336a5d99e4f02f3b3fea4123178b9daa60
Nomas <nomasx2000@gmail.com>
2025-09-25 21:11:11 +0300

    Added more SPC vests for Clear Sky

--- addons/clearsky/CfgWeapons.hpp
+++ addons/clearsky/CfgWeapons.hpp
@@ -1,5 +1,6 @@
 class cfgWeapons
 {
+	// VESTS
 	class rhsusf_spc_patchless;
 	class GVAR(V_spc_patchless): rhsusf_spc_patchless
 	{
@@ -13,13 +14,234 @@ class cfgWeapons
 			"camo3"
 		};
 		hiddenSelectionsTextures[] = {
-			QPATHTOF(data\rhs_spc02_csky_co.paa),
+			QPATHTOF(data\rhs_spc01_csky_co.paa),
 			QPATHTOF(data\rhs_spc_gear1_csky_co.paa),
 			QPATHTOF(data\rhs_spc_gear2_csky_co.paa),
 			QPATHTOF(data\rhs_usmc_camelbak_csky_co.paa)
 		};
 	};
 
+	class rhsusf_spc;
+	class GVAR(V_spc): rhsusf_spc
+	{
+		author = "RHS, Rad";
+		displayName = "SPC [Clearsky]";
+		
+        hiddenSelections[] = {
+			"camo",
+			"camo1",
+			"camo2",
+			"camo3"
+		};
+		hiddenSelectionsTextures[] = {
+			QPATHTOF(data\rhs_spc01_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear1_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear2_csky_co.paa),
+			QPATHTOF(data\rhs_usmc_camelbak_csky_co.paa)
+		};
+	};
+
+	class rhsusf_spc_corpsman;
+	class GVAR(V_spc_corpsman): rhsusf_spc_corpsman
+	{
+		author = "RHS, Rad";
+		displayName = "SPC (Corpsman) [Clearsky]";
+		
+        hiddenSelections[] = {
+			"camo",
+			"camo1",
+			"camo2",
+			"camo3"
+		};
+		hiddenSelectionsTextures[] = {
+			QPATHTOF(data\rhs_spc01_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear1_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear2_csky_co.paa),
+			QPATHTOF(data\rhs_usmc_camelbak_csky_co.paa)
+		};
+	};
+
+	class rhsusf_spc_crewman;
+	class GVAR(V_spc_crewman): rhsusf_spc_crewman
+	{
+		author = "RHS, Rad";
+		displayName = "SPC (Crewman) [Clearsky]";
+		
+        hiddenSelections[] = {
+			"camo",
+			"camo1",
+			"camo2",
+			"camo3"
+		};
+		hiddenSelectionsTextures[] = {
+			QPATHTOF(data\rhs_spc01_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear1_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear2_csky_co.paa),
+			QPATHTOF(data\rhs_usmc_camelbak_csky_co.paa)
+		};
+	};
+
+	class rhsusf_spc_iar;
+	class GVAR(V_spc_iar): rhsusf_spc_iar
+	{
+		author = "RHS, Rad";
+		displayName = "SPC (IAR) [Clearsky]";
+		
+        hiddenSelections[] = {
+			"camo",
+			"camo1",
+			"camo2",
+			"camo3"
+		};
+		hiddenSelectionsTextures[] = {
+			QPATHTOF(data\rhs_spc01_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear1_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear2_csky_co.paa),
+			QPATHTOF(data\rhs_usmc_camelbak_csky_co.paa)
+		};
+	};
+
+	class rhsusf_spc_light;
+	class GVAR(V_spc_light): rhsusf_spc_light
+	{
+		author = "RHS, Rad";
+		displayName = "SPC (Light) [Clearsky]";
+		
+        hiddenSelections[] = {
+			"camo",
+			"camo1",
+			"camo2",
+			"camo3"
+		};
+		hiddenSelectionsTextures[] = {
+			QPATHTOF(data\rhs_spc01_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear1_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear2_csky_co.paa),
+			QPATHTOF(data\rhs_usmc_camelbak_csky_co.paa)
+		};
+	};
+
+	class rhsusf_spc_mg;
+	class GVAR(V_spc_mg): rhsusf_spc_mg
+	{
+		author = "RHS, Rad";
+		displayName = "SPC (Machinegunner) [Clearsky]";
+		
+        hiddenSelections[] = {
+			"camo",
+			"camo1",
+			"camo2",
+			"camo3"
+		};
+		hiddenSelectionsTextures[] = {
+			QPATHTOF(data\rhs_spc01_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear1_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear2_csky_co.paa),
+			QPATHTOF(data\rhs_usmc_camelbak_csky_co.paa)
+		};
+	};
+
+	class rhsusf_spc_marksman;
+	class GVAR(V_spc_marksman): rhsusf_spc_marksman
+	{
+		author = "RHS, Rad";
+		displayName = "SPC (Marksman) [Clearsky]";
+		
+        hiddenSelections[] = {
+			"camo",
+			"camo1",
+			"camo2",
+			"camo3"
+		};
+		hiddenSelectionsTextures[] = {
+			QPATHTOF(data\rhs_spc01_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear1_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear2_csky_co.paa),
+			QPATHTOF(data\rhs_usmc_camelbak_csky_co.paa)
+		};
+	};
+
+	class rhsusf_spc_patchless_radio;
+	class GVAR(V_spc_patchless_radio): rhsusf_spc_patchless_radio
+	{
+		author = "RHS, Rad";
+		displayName = "SPC (Patchless/Radio) [Clearsky]";
+		
+        hiddenSelections[] = {
+			"camo",
+			"camo1",
+			"camo2",
+			"camo3"
+		};
+		hiddenSelectionsTextures[] = {
+			QPATHTOF(data\rhs_spc01_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear1_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear2_csky_co.paa),
+			QPATHTOF(data\rhs_usmc_camelbak_csky_co.paa)
+		};
+	};
+
+	class rhsusf_spc_rifleman;
+	class GVAR(V_spc_rifleman): rhsusf_spc_rifleman
+	{
+		author = "RHS, Rad";
+		displayName = "SPC (Rifleman) [Clearsky]";
+		
+        hiddenSelections[] = {
+			"camo",
+			"camo1",
+			"camo2",
+			"camo3"
+		};
+		hiddenSelectionsTextures[] = {
+			QPATHTOF(data\rhs_spc01_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear1_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear2_csky_co.paa),
+			QPATHTOF(data\rhs_usmc_camelbak_csky_co.paa)
+		};
+	};
+
+	class rhsusf_spc_sniper;
+	class GVAR(V_spc_sniper): rhsusf_spc_sniper
+	{
+		author = "RHS, Rad";
+		displayName = "SPC (Scout Sniper) [Clearsky]";
+		
+        hiddenSelections[] = {
+			"camo",
+			"camo1",
+			"camo2",
+			"camo3"
+		};
+		hiddenSelectionsTextures[] = {
+			QPATHTOF(data\rhs_spc01_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear1_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear2_csky_co.paa),
+			QPATHTOF(data\rhs_usmc_camelbak_csky_co.paa)
+		};
+	};
+
+	class rhsusf_spc_squadleader;
+	class GVAR(V_spc_squadleader): rhsusf_spc_squadleader
+	{
+		author = "RHS, Rad";
+		displayName = "SPC (Squadleader) [Clearsky]";
+		
+        hiddenSelections[] = {
+			"camo",
+			"camo1",
+			"camo2",
+			"camo3"
+		};
+		hiddenSelectionsTextures[] = {
+			QPATHTOF(data\rhs_spc01_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear1_csky_co.paa),
+			QPATHTOF(data\rhs_spc_gear2_csky_co.paa),
+			QPATHTOF(data\rhs_usmc_camelbak_csky_co.paa)
+		};
+	};
+
+	// HELMETS
 	class rhs_altyn_novisor_ess;
 	class GVAR(H_altyn_novisor_ess_black): rhs_altyn_novisor_ess {
 		author = "RHS, Rad";
